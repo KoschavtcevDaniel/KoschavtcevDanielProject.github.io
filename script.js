f6846ef167268ef46db5681ec6e0f812363bd947
KoschavtcevDaniel <112947715+KoschavtcevDaniel@users.noreply.github.com>
2022-12-12 22:03:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
+
+$(function(){
+    $(".ajaxForm").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+                    alert("Форма отправлена");
+                }else{
+                    alert("Ошибка: " + response.message);
+                }
+            }
+        });
+    });
+});
